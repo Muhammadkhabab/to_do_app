c7f3f40f00f1911c702e1fa16aa0e852f9faac0e
Query Byte Tech <khababkhattak332@gmail.com>
2024-05-23 14:51:50 +0500

    first screen design task screen

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskScreen extends StatefulWidget {
@@ -10,32 +11,80 @@ class TaskScreen extends StatefulWidget {
 class _TaskScreenState extends State<TaskScreen> {
   @override
   Widget build(BuildContext context) {
+    int counter = 0;
+    bool isSaving = false;
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: const EdgeInsets.only(
-          top: 60,
-          left: 30,
-          right: 30,
-          bottom: 30,
-        ),
-        child: const Column(
-          children: [
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Icon(
-                Icons.list,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+            isSaving = true;
+          });
+          print(counter);
+        },
+        backgroundColor: Colors.lightBlueAccent,
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add, color: Colors.white,),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 60,
+              left: 30,
+              right: 30,
+              bottom: 30,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.list,
+                    size: 40,
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    letterSpacing: 2,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '$counter Task',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  )),
+              child: const Center(
+                child: Text('List View Builder For The Task'),
               ),
             ),
-            Text(
-              'Todo',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Colors.white,
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
